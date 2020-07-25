8d5edb28ceb447b97fec3af30ac66c14c0fcd6bf
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-25 02:18:18 -0500

    updates and goodnight

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,13 @@
 import Header from '../components/fixed-header';
 
-const Contact = () => {
+interface Props {
+	props: string | number;
+}
+
+const Contact = ({ props }: Props) => {
 	return (
 		<>
-			<Header />
+			<Header props={props} />
 			<div className='p-4 shadow rounded bg-white'>
 				<h1 className='text-purple-500 leading-normal'>Next.js</h1>
 				<p className='text-gray-500'>with Tailwind CSS</p>
