d13abdbb442881685069b8c78abd3cd7d1213d24
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-26 23:51:45 -0500

    configured fortawesome/fontawesome in root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,19 +9,26 @@ import {
 	faSnapchat,
 	faTwitter
 } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { AppProps } from 'next/app';
 
+config.autoAddCss = false;
+library.add(
+	faBars,
+	faFacebook,
+	faGithub,
+	faGoogle,
+	faLinkedin,
+	faSnapchat,
+	faTwitter
+);
+
 function App({ Component, pageProps }: AppProps) {
-	config.autoAddCss = false;
-	library.add(
-		faFacebook,
-		faGithub,
-		faGoogle,
-		faLinkedin,
-		faSnapchat,
-		faTwitter
-	);
 	return <Component {...pageProps} />;
 }
 
 export default App;
+
+// Configuring Fortawesome with nextjs v9+
+// https://github.com/UnlyEd/next-right-now/blob/master/src/pages/_app.tsx
+// https://stackoverflow.com/questions/44752189/how-to-add-font-awesome-to-next-js-project
