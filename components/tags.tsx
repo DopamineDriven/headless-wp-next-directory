2200c46b9b0d78b6c3491c64522f4b52c4adabd3
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-29 03:22:16 -0500

    tags built

---
+++ components/tags.tsx
@@ -0,0 +1,16 @@
+const Tags = ({ tags }: any) => {
+	return (
+		<div className='max-w-2xl mx-auto'>
+			<p className='mt-8 text-lg font-bold'>
+				Tagged
+				{tags.edges.map((tag: any, index: any) => (
+					<span key={index} className='ml-4 font-normal'>
+						{tag.node.name}
+					</span>
+				))}
+			</p>
+		</div>
+	);
+};
+
+export default Tags;
