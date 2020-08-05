684da13fac42acf00cbd48a630dd38c8934c2818
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-05 08:42:49 -0500

    removed faSort from fontawesome import

--- components/search-box.tsx
+++ components/search-box.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
-import { faSort, faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 // https://tailwindtemplates.io/search/#230
 
