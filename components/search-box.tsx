0d1a9f7e54869c5df49e3903f6f4a39a83747448
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-05 08:43:15 -0500

    made import of fontawesome icon more precise

--- components/search-box.tsx
+++ components/search-box.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
-import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons/faSortDown';
 
 // https://tailwindtemplates.io/search/#230
 
