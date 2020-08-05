87cd0121d39ed8388bdb7c6c3b87697c1302c833
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-04 20:06:34 -0500

    updates to categories

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,34 +1,30 @@
-type CategoriesProps = {
-	categories: {
-		edges: any;
-	} | any;
-};
+// type CategoriesProps = {
+// 	categories: {
+// 		edges: any;
+// 	} | any;
+// };
 
-type CategoryProps = {
-	category: any;
-	index: any;
-}
+// type CategoryProps = {
+// 	category: any;
+// 	index: any;
+// }
 
-
-export default function Categories({ categories }: CategoriesProps) {
+export default function Categories({ categories }: any) {
 	return (
 		<span className='ml-1'>
 			under
 			{categories.edges.length > 0 ? (
-				categories.edges.map(({ category, index }: CategoryProps) => {
-					return (
-						<span key={index} className='ml-1 text-success'>
-							&num;{category.node.name}
-						</span>
-					);
-				})
+				categories.edges.map((category: any, index: any) => (
+					<span key={index} className='ml-1 text-success'>
+						&num;{category.node.name}
+					</span>
+				))
 			) : (
 				<span className='ml-1'>{categories.edges.node.name}</span>
 			)}
 		</span>
 	);
 }
-
 
 /*
 type CategoriesProps = {
