a0a2ffc24efb6c0bf5673d0a0e8d2d3c9b7a8cc0
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-09 09:48:53 -0500

    will review with James at 1400 meeting today; mutations likely requried for multifaceted global sorting/filtering events

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,51 +1,51 @@
 import { CMS_NAME, CMS_URL } from '../lib/constants';
 import Link from 'next/link';
-import {
-	getAllPostsForHomeDateAsc,
-	getAllPostsForHomeDateDesc,
-	getAllPostsForHomeAlphabetical,
-	getAllPostsForHomeReverseAlphabetical
-} from '../lib/api';
+// import {
+// 	getAllPostsForHomeDateAsc,
+// 	getAllPostsForHomeDateDesc,
+// 	getAllPostsForHomeAlphabetical,
+// 	getAllPostsForHomeReverseAlphabetical
+// } from '../lib/api';
 
-type Sorting = {
-	sort: Promise<any>;
-	title: string;
-};
+// type Sorting = {
+// 	sort: any;
+// 	title: string;
+// };
 
-const handleClick = (e: Event) => {
-	e.preventDefault();
-};
+// const handleClick = (e: Event) => {
+// 	e.preventDefault();
+// };
 
-const sorts: Sorting[] = [
-	{
-		sort: getAllPostsForHomeDateAsc(),
-		title: 'date ascending (most recent)'
-	},
-	{
-		sort: getAllPostsForHomeDateDesc(),
-		title: 'date descending (least recent)'
-	},
-	{
-		sort: getAllPostsForHomeAlphabetical(),
-		title: 'alphabetical order'
-	},
-	{
-		sort: getAllPostsForHomeReverseAlphabetical(),
-		title: 'reverse-alphabetical order'
-	}
-];
+// const sorts: Sorting[] = [
+// 	{
+// 		sort: getAllPostsForHomeDateAsc(),
+// 		title: 'date ascending (most recent)'
+// 	},
+// 	{
+// 		sort: getAllPostsForHomeDateDesc(),
+// 		title: 'date descending (least recent)'
+// 	},
+// 	{
+// 		sort: getAllPostsForHomeAlphabetical(),
+// 		title: 'alphabetical order'
+// 	},
+// 	{
+// 		sort: getAllPostsForHomeReverseAlphabetical(),
+// 		title: 'reverse-alphabetical order'
+// 	}
+// ];
 
 export default function Intro() {
-	const sortingMap = sorts.map(sorting => (
-		<button
-			key={sorting.title}
-			className='mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-500 transition-colors mb-6 lg:mb-0 rounded'
-			aria-label='Documentation'
-			onClick={() => sorting.sort}
-		>
-			{sorting.title}
-		</button>
-	));
+	// const sortingMap = sorts.map(sorting => (
+	// 	<button
+	// 		key={sorting.title}
+	// 		className='mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-500 transition-colors mb-6 lg:mb-0 rounded'
+	// 		aria-label='Documentation'
+	// 		onClick={() => sorting.sort}
+	// 	>
+	// 		{sorting.title}
+	// 	</button>
+	// ));
 	return (
 		<>
 			<h1 className='text-6xl sm:text-6xl xs:text-5xl font-bold text-center justify-center font-body tracking-tight leading-tight mt-4'>
@@ -119,9 +119,9 @@ export default function Intro() {
 			<h2 className='text-2xl sm:text-2xl xs:text-2xl font-bold text-center justify-center font-body tracking-tight leading-tight mt-4'>
 				Sort Directory by Company Title or Date Published
 			</h2>
-			<div className='grid-cols-4 inline-block px-4 py-2 justify-center items-center align-middle'>
+			{/* <div className='grid-cols-4 inline-block px-4 py-2 justify-center items-center align-middle'>
 				{sortingMap}
-			</div>
+			</div> */}
 		</>
 	);
 }
