df96535ce490d8f1d8e9894b9ae51dfda71ebad3
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-09 08:33:35 -0500

    updates to components; refining UI

--- components/tags.tsx
+++ components/tags.tsx
@@ -6,12 +6,11 @@ type TagProps = {
 
 export default function Tags({ tags }: TagProps) {
 	return (
-		<div className='max-w-2xl mx-auto'>
+		<div className='max-w-2xl mx-auto text-center'>
 			<p className='mt-8 text-lg font-bold'>
-				Tagged
 				{tags.edges.map((tag: any, index: any) => (
-					<span key={index} className='ml-4 font-normal'>
-						{tag.node.name}
+					<span key={index} className='inline-block bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2'>
+						#{tag.node.name}
 					</span>
 				))}
 			</p>
