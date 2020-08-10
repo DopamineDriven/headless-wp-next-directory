57ef4f8b2a7b378f2bc4aa8266f683798e0edc71
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-10 00:22:21 -0500

    global refactoring, massive facelift incoming

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,94 @@ import Link from 'next/link';
 import SvgLogo from './svg-logo-only';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import HeaderSVG from './header-svg';
+
+interface NavRef {
+	href: string;
+	label: string;
+}
+
+type HeaderProps = {
+	props: string | number;
+};
+
+const links: NavRef[] = [
+	{
+		href: '/',
+		label: 'Directory'
+	},
+	{
+		href: '/contact',
+		label: 'Contact'
+	},
+	{
+		href: '/about',
+		label: 'About'
+	}
+];
+
+const Header = ({ props }: HeaderProps) => {
+	const [navOpen, setNavOpen] = useState(false);
+	const navlist = links.map(link => (
+		<li className='nav-item' key={`${link.href}-${link.label}`}>
+			<Link href={link.href}>
+				<a
+					className='px-3 py-2 flex items-center text-lg uppercase font-bold leading-snug text-black hover:opacity-75'
+					aria-label={link.label}
+				>
+					{link.label}
+				</a>
+			</Link>
+		</li>
+	));
+	return (
+		<nav className='relative grid w-full cover overflow-x-hidden overflow-y-hidden mb-10'>
+			<a className='bg-cover grid grid-flow-row w-full'>
+				<img
+					src={'/assets/headerhero.jpg'}
+					className='overflow-x-hidden overflow-y-hidden bg-cover flex relative'
+				/>
+			</a>
+
+			<div className='container flex items-center justify-between'>
+				<div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
+					<Link href='/'>
+						<a>{<SvgLogo props={props} />}</a>
+					</Link>
+					<button
+						className='text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
+						type='button'
+						onClick={() => setNavOpen(!navOpen)}
+						name='open-nav-button'
+					>
+						<a aria-label='open-nav'>
+							<FontAwesomeIcon icon={faBars} />
+						</a>
+					</button>
+				</div>
+				<div
+					className={
+						'lg:flex flex-grow items-right' +
+						(navOpen ? ' flex float-right' : ' hidden')
+					}
+				>
+					<ul className='flex flex-col lg:flex-row list-none lg:ml-auto font-body'>
+						{navlist}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Header;
+
+/*
+import { useState } from 'react';
+import Link from 'next/link';
+import SvgLogo from './svg-logo-only';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 interface NavRef {
 	href: string;
@@ -76,3 +164,5 @@ const Header = ({ props }: HeaderProps) => {
 };
 
 export default Header;
+
+*/
