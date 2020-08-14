12b7b0dea2e2f08d29960724bababe57b39ca81a
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-14 06:50:34 -0500

    building out search feature

--- components/custom-select.tsx
+++ components/custom-select.tsx
@@ -18,13 +18,13 @@ enum Order {
 interface CustomDropDown {
 	field: string | string[];
 	order: string | string[];
-	preview?: boolean;
+	color: string;
 }
 
 const { TITLE, MODIFIED, DATE } = Field;
 const { ASC, DESC } = Order;
 
-const CustomSelect = ({ field, order, preview }: CustomDropDown) => {
+const CustomSelect = ({ color, field, order }: CustomDropDown) => {
 	const [dropdownPopoverShow, setDropdownPopoverShow] = useState(false);
 	const btnDropdownRef = useRef(null);
 	const popoverDropdownRef = useRef(null);
@@ -38,9 +38,53 @@ const CustomSelect = ({ field, order, preview }: CustomDropDown) => {
 		);
 		setDropdownPopoverShow(true);
 	};
+	const closeDropdownPopover = () => {
+		setDropdownPopoverShow(false);
+	};
+
+	let bgColor;
+	color === 'white' ? (bgColor = 'bg-customGray') : (bgColor = `bg-${color}`);
+
+	return (
+		<>
+			<div className='flex flex-wrap'>
+				<div className='w-full sm:w-6/12 md:w-4/12 px-4'>
+					<div className='relative inline-flex align-middle w-full'>
+						<button
+							className={`text-white font-bold font-polished uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ${bgColor}`}
+							style={{ transition: 'all .15s ease' }}
+							type='button'
+							ref={btnDropdownRef}
+							onClick={() => {
+								dropdownPopoverShow
+									? closeDropdownPopover()
+									: openDropdownPopover();
+							}}
+						>
+							{color === 'white' ? 'FIELDS' : `FIELD`}
+						</button>
+						<div
+							ref={popoverDropdownRef}
+							className={
+								(dropdownPopoverShow ? 'block' : 'hidden ') +
+								(color === 'white' ? 'bg-white ' : bgColor + ' ') +
+								'text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1 w-48 min-w-full'
+							}
+						>
+							<a
+								className={
+									'text-sm py-2 px-4 font-polished block w-full whitespace-no-wrap bg-transparent' +
+									(color === 'white' ? 'black' : 'text-white')
+								}
+							></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default CustomSelect;
-
 
 // https://medium.com/javascript-in-plain-english/usepopper-with-styled-components-for-react-react-popper-2-568284d029bf
