7cf5bca5d167eb33181bb9b57255ad30c08fe287
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-14 06:24:09 -0500

    building out search feature

--- components/custom-select.tsx
+++ components/custom-select.tsx
@@ -1,7 +1,8 @@
 // @flow
-import { createPopper } from '@popperjs/core';
-import type { StrictModifiers } from '@popperjs/core';
+import { usePopper } from 'react-popper';
+import { StrictModifiers } from '@popperjs/core';
 import { getAllPostsForHomeSorted } from '../lib/api';
+import { useState, useRef } from 'react';
 
 enum Field {
 	TITLE = 'TITLE',
@@ -17,12 +18,29 @@ enum Order {
 interface CustomDropDown {
 	field: string | string[];
 	order: string | string[];
-	preview: boolean;
+	preview?: boolean;
 }
 
 const { TITLE, MODIFIED, DATE } = Field;
 const { ASC, DESC } = Order;
 
-const CustomSelect = () => {};
+const CustomSelect = ({ field, order, preview }: CustomDropDown) => {
+	const [dropdownPopoverShow, setDropdownPopoverShow] = useState(false);
+	const btnDropdownRef = useRef(null);
+	const popoverDropdownRef = useRef(null);
+	const openDropdownPopover = () => {
+		usePopper<StrictModifiers>(
+			btnDropdownRef.current,
+			popoverDropdownRef.current,
+			{
+				placement: 'bottom-start'
+			}
+		);
+		setDropdownPopoverShow(true);
+	};
+};
 
 export default CustomSelect;
+
+
+// https://medium.com/javascript-in-plain-english/usepopper-with-styled-components-for-react-react-popper-2-568284d029bf
