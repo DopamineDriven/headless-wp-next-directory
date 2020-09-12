66c6d4de49855ff745d2f74487948bb03bcd4e1d
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-12 05:49:42 -0500

    configuring google analytics; lib/google-analytics.ts and pages/_app.tsx complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,23 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { AppProps, NextWebVitalsMetric } from 'next/app';
 import { ReactElement, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import * as gtag from 'lib/google-analytics';
 
 config.autoAddCss = false;
 
 function App({ Component, pageProps }: AppProps): ReactElement {
+	const router = useRouter();
+	const { logPageView } = gtag;
+	useEffect(() => {
+		const handleRouteChange = () => {
+			logPageView();
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
 	return <Component {...pageProps} />;
 }
 
@@ -16,7 +28,3 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
 }
 
 export default App;
-
-// Configuring Fortawesome with nextjs v9+
-// https://github.com/UnlyEd/next-right-now/blob/master/src/pages/_app.tsx
-// https://stackoverflow.com/questions/44752189/how-to-add-font-awesome-to-next-js-project
