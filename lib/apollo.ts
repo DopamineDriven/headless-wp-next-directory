94056b08023c063655862ca5367ce1fa56b73946
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-28 11:52:20 -0500

    apollo incorporation underway

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -8,3 +8,28 @@ import {
 import { concatPagination } from '@apollo/client/utilities';
 
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
+
+function createApolloClient() {
+	const headers = { 'Content-Type': 'application/json' };
+	return new ApolloClient({
+		ssrMode: typeof window === 'undefined',
+		connectToDevTools: true,
+		link: new HttpLink({
+			uri: `${process.env.WORDPRESS_API_URL}`,
+			credentials: `${headers}`
+		}),
+		cache: new InMemoryCache({
+			addTypename: true,
+			resultCaching: true,
+			typePolicies: {
+				Query: {
+					fields: {
+						posts: concatPagination()
+					}
+				}
+			}
+		})
+	});
+}
+
+export function initializeApollo(initialState: any = null) {}
