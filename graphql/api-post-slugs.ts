7737dc32005e891a8c7d0939a7cf3cf5a76ada9f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-10-12 03:29:37 -0500

    cross-var and dotenv-cli for safe package.json scripting

--- graphql/api-post-slugs.ts
+++ graphql/api-post-slugs.ts
@@ -1,21 +1,15 @@
 import { gql } from '@apollo/client';
-import { fetchAPI } from '../lib/api';
 
-const GET_ALL_POSTS_WITH_SLUG = async () => {
-	const postSlugs = await fetchAPI(
-		gql`
-			query getAllPostsWithSlug {
-				posts(first: 10000) {
-					edges {
-						node {
-							slug
-						}
-					}
+const POST_SLUGS = gql`
+	query getAllPostsWithSlug {
+		posts(first: 10000) {
+			edges {
+				node {
+					slug
 				}
 			}
-		`
-	);
-	return postSlugs?.posts.edges.node?.slug;
-};
+		}
+	}
+`;
 
-export default GET_ALL_POSTS_WITH_SLUG;
+export default POST_SLUGS;
