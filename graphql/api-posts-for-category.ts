6591596ba1f2f594e87aa85c770ec19ef335ab31
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-26 08:48:08 -0500

    added psuedoObj to be called when null checking for posts from categories

--- graphql/api-posts-for-category.ts
+++ graphql/api-posts-for-category.ts
@@ -1,4 +1,8 @@
 import { gql } from '@apollo/client';
+import {
+	AllPostsForCategory_categories_edges_node_posts,
+	AllPostsForCategory_categories_edges_node_posts_nodes
+} from './__generated__/AllPostsForCategory';
 
 export const ALL_POSTS_FOR_CATEGORY = gql`
 	query AllPostsForCategory($first: Int, $name: [String]) {
@@ -51,3 +55,21 @@ export const allPostsForCategoryQueryVariables = {
 };
 
 export const allPostsForCategoryKeyNameForCache = `categories({"first":${allPostsForCategoryQueryVariables.first},"where":{"name":${allPostsForCategoryQueryVariables.name}}})`;
+
+export const PsuedoObj_AllPostsForCategory_categories_edges_node_posts: AllPostsForCategory_categories_edges_node_posts = {
+	__typename: 'CategoryToPostConnection',
+	nodes: []
+};
+
+export const PsuedoObj_AllPostsForCategory_categories__edges_node_posts_nodes: AllPostsForCategory_categories_edges_node_posts_nodes = {
+	__typename: 'Post',
+	id: 'nullObject',
+	title: null,
+	date: null,
+	excerpt: null,
+	slug: null,
+	modified: null,
+	social: null,
+	featuredImage: null,
+	author: null
+};
