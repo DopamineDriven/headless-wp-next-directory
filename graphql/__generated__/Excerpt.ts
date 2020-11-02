f05d45fc6b6853668016967e43d1e56138005249
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-02 10:02:26 -0600

    ran codegen

--- graphql/__generated__/Excerpt.ts
+++ graphql/__generated__/Excerpt.ts
@@ -8,32 +8,32 @@
 // ====================================================
 
 export interface Excerpt_posts_edges_node {
-	__typename: 'Post';
-	/**
-	 * The excerpt of the post.
-	 */
-	excerpt: string | null;
+  __typename: "Post";
+  /**
+   * The excerpt of the post.
+   */
+  excerpt: string | null;
 }
 
 export interface Excerpt_posts_edges {
-	__typename: 'RootQueryToPostConnectionEdge';
-	/**
-	 * The item at the end of the edge
-	 */
-	node: Excerpt_posts_edges_node | null;
+  __typename: "RootQueryToPostConnectionEdge";
+  /**
+   * The item at the end of the edge
+   */
+  node: Excerpt_posts_edges_node | null;
 }
 
 export interface Excerpt_posts {
-	__typename: 'RootQueryToPostConnection';
-	/**
-	 * Edges for the RootQueryToPostConnection connection
-	 */
-	edges: (Excerpt_posts_edges | null)[] | null;
+  __typename: "RootQueryToPostConnection";
+  /**
+   * Edges for the RootQueryToPostConnection connection
+   */
+  edges: (Excerpt_posts_edges | null)[] | null;
 }
 
 export interface Excerpt {
-	/**
-	 * Connection between the RootQuery type and the post type
-	 */
-	posts: Excerpt_posts | null;
+  /**
+   * Connection between the RootQuery type and the post type
+   */
+  posts: Excerpt_posts | null;
 }
--- graphql/__generated__/Title.ts
+++ graphql/__generated__/Title.ts
@@ -8,32 +8,32 @@
 // ====================================================
 
 export interface Title_posts_edges_node {
-	__typename: 'Post';
-	/**
-	 * The title of the post. This is currently just the raw title. An amendment to support rendered title needs to be made.
-	 */
-	title: string | null;
+  __typename: "Post";
+  /**
+   * The title of the post. This is currently just the raw title. An amendment to support rendered title needs to be made.
+   */
+  title: string | null;
 }
 
 export interface Title_posts_edges {
-	__typename: 'RootQueryToPostConnectionEdge';
-	/**
-	 * The item at the end of the edge
-	 */
-	node: Title_posts_edges_node | null;
+  __typename: "RootQueryToPostConnectionEdge";
+  /**
+   * The item at the end of the edge
+   */
+  node: Title_posts_edges_node | null;
 }
 
 export interface Title_posts {
-	__typename: 'RootQueryToPostConnection';
-	/**
-	 * Edges for the RootQueryToPostConnection connection
-	 */
-	edges: (Title_posts_edges | null)[] | null;
+  __typename: "RootQueryToPostConnection";
+  /**
+   * Edges for the RootQueryToPostConnection connection
+   */
+  edges: (Title_posts_edges | null)[] | null;
 }
 
 export interface Title {
-	/**
-	 * Connection between the RootQuery type and the post type
-	 */
-	posts: Title_posts | null;
+  /**
+   * Connection between the RootQuery type and the post type
+   */
+  posts: Title_posts | null;
 }
