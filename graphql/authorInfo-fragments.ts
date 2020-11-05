1f33e55ef537c9a11de82a5751777fb5ebe6c606
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-05 17:40:37 -0600

    created authorInfo-fragmenet for graphql queries

---
+++ graphql/authorInfo-fragments.ts
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+ export const FRAGMENT_AUTHOR_FIELDS = gql`
+     fragment authorFields on User {
+        name
+        firstName
+        lastName
+        avatar {
+            url
+            size
+            height
+            width
+            }
+        }
+    `;
