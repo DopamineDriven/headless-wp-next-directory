fa14d028c1d9f9602658ad3adfccbd67eb115a3b
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-17 05:54:11 -0600

    changed getAllPostsWithSlug slightly

--- graphql/__generated__/getAllPostsWithSlug.ts
+++ graphql/__generated__/getAllPostsWithSlug.ts
@@ -4,10 +4,10 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
-// GraphQL query operation: getAllPostsWithSlug
+// GraphQL query operation: GetAllPostsWithSlug
 // ====================================================
 
-export interface getAllPostsWithSlug_posts_edges_node {
+export interface GetAllPostsWithSlug_posts_edges_node {
   __typename: "Post";
   /**
    * The uri slug for the post. This is equivalent to the WP_Post-&gt;post_name field and the post_name column in the database for the &quot;post_objects&quot; table.
@@ -15,25 +15,25 @@ export interface getAllPostsWithSlug_posts_edges_node {
   slug: string | null;
 }
 
-export interface getAllPostsWithSlug_posts_edges {
+export interface GetAllPostsWithSlug_posts_edges {
   __typename: "RootQueryToPostConnectionEdge";
   /**
    * The item at the end of the edge
    */
-  node: getAllPostsWithSlug_posts_edges_node | null;
+  node: GetAllPostsWithSlug_posts_edges_node | null;
 }
 
-export interface getAllPostsWithSlug_posts {
+export interface GetAllPostsWithSlug_posts {
   __typename: "RootQueryToPostConnection";
   /**
    * Edges for the RootQueryToPostConnection connection
    */
-  edges: (getAllPostsWithSlug_posts_edges | null)[] | null;
+  edges: (GetAllPostsWithSlug_posts_edges | null)[] | null;
 }
 
-export interface getAllPostsWithSlug {
+export interface GetAllPostsWithSlug {
   /**
    * Connection between the RootQuery type and the post type
    */
-  posts: getAllPostsWithSlug_posts | null;
+  posts: GetAllPostsWithSlug_posts | null;
 }
